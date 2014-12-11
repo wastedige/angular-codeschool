86dadb0a8acb2c05a09d6a4a18687e9f547a64a1
Shaahin Shahbazi <shaahin@gmail.com>
2014-12-11 10:32:47 -0600

    ng-repeat

--- app/app.js
+++ app/app.js
@@ -1,7 +1,13 @@
-(function(){
-  var gem = { name: 'Azurite', price: 2.95 };
+(function() {
   var app = angular.module('gemStore', []);
+
   app.controller('StoreController', function(){
-    this.product = gem;
+   	this.products = gems; 
   });
+
+  var gems = [
+    { name: 'Azurite', price: 2.95 },
+    { name: 'Bloodstone', price: 5.95 },
+    { name: 'Zircon', price: 3.95 },
+  ];
 })();
