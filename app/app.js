2b86244d1f36f47af06b4a6592630969664f0510
Shaahin Shahbazi <shaahin@gmail.com>
2014-12-12 11:12:33 -0600

    Using an Element Directive

--- app/app.js
+++ app/app.js
@@ -1,38 +1,47 @@
 (function() {
   var app = angular.module('gemStore', []);
 
-  app.controller('StoreController', function(){
+  app.controller('GalleryController', function(){
+    this.current = 0;
+    this.setCurrent = function(imageNumber){
+      this.current = imageNumber || 0;
+    };
+  });
+
+  app.controller('StoreController', function() {
     this.products = gems;
   });
 
-  app.controller('TabController', function(){
+  app.controller("TabController", function() {
     this.tab = 1;
 
-    this.setTab = function(tab){
-      this.tab = tab;
+    this.isSet = function(checkTab) {
+      return this.tab === checkTab;
     };
 
-    this.isSet = function(tab){
-      return (this.tab === tab);
-    };
-  });
-
-  app.controller('GalleryController', function(){
-    this.current = 0;
-
-    this.setCurrent = function(index){
-      this.current = index;
+    this.setTab = function(setTab) {
+      this.tab = setTab;
     };
   });
 
-  app.controller('ReviewController', function(){
+  app.controller("ReviewController", function(){
+
     this.review = {};
-    
+
     this.addReview = function(product){
-    	product.reviews.push(this.review);
-		this.review = {};      
+      product.reviews.push(this.review);
+      this.review = {};
     };
+
   });
+	
+	// Element directive!
+  app.directive("productDescription", function() {
+ 	 return {
+    restrict: 'E',
+    templateUrl: "product-description.html"
+  };
+});
 
   var gems = [
     {
@@ -51,13 +60,11 @@
       reviews: [{
         stars: 5,
         body: "I love this gem!",
-        author: "joe@example.org",
-        createdOn: 1397490980837
+        author: "joe@example.org"
       }, {
         stars: 1,
         body: "This gem sucks.",
-        author: "tim@example.org",
-        createdOn: 1397490980837
+        author: "tim@example.org"
       }]
     }, {
       name: 'Bloodstone',
@@ -70,48 +77,43 @@
       images: [
         "images/gem-01.gif",
         "images/gem-03.gif",
-        "images/gem-04.gif",
+        "images/gem-04.gif"
       ],
       reviews: [{
         stars: 3,
         body: "I think this gem was just OK, could honestly use more shine, IMO.",
-        author: "JimmyDean@example.org",
-        createdOn: 1397490980837
+        author: "JimmyDean@example.org"
       }, {
         stars: 4,
         body: "Any gem with 12 faces is for me!",
-        author: "gemsRock@example.org",
-        createdOn: 1397490980837
+        author: "gemsRock@example.org"
       }]
-    }, {
-      name: 'Zircon',
-      description: "Zircon is our most coveted and sought after gem. You will pay much to be the proud owner of this gorgeous and high shine gem.",
-      shine: 70,
-      price: 1100,
-      rarity: 2,
-      color: '#000',
-      faces: 6,
-      images: [
-        "images/gem-06.gif",
-        "images/gem-07.gif",
-        "images/gem-08.gif"
-      ],
-      reviews: [{
-        stars: 1,
-        body: "This gem is WAY too expensive for its rarity value.",
-        author: "turtleguyy@example.org",
-        createdOn: 1397490980837
       }, {
-        stars: 1,
-        body: "BBW: High Shine != High Quality.",
-        author: "LouisW407@example.org",
-        createdOn: 1397490980837
-      }, {
-        stars: 1,
-        body: "Don't waste your rubles!",
-        author: "nat@example.org",
-        createdOn: 1397490980837
-      }]
+        name: 'Zircon',
+        description: "Zircon is our most coveted and sought after gem. You will pay much to be the proud owner of this gorgeous and high shine gem.",
+        shine: 70,
+        price: 1100,
+        rarity: 2,
+        color: '#000',
+        faces: 6,
+        images: [
+          "images/gem-06.gif",
+          "images/gem-07.gif",
+          "images/gem-08.gif"
+        ],
+        reviews: [{
+          stars: 1,
+          body: "This gem is WAY too expensive for its rarity value.",
+          author: "turtleguyy@example.org"
+        }, {
+          stars: 1,
+          body: "BBW: High Shine != High Quality.",
+          author: "LouisW407@example.org"
+        }, {
+          stars: 1,
+          body: "Don't waste your rubles!",
+          author: "nat@example.org"
+        }]
     }
   ];
 })();
